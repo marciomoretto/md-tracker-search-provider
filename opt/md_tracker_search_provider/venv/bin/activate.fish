818b44c86a30bb57f8861a234320e6d933107877
Márcio Moretto <marcio.mr@gmail.com>
2024-11-18 23:56:23 -0300

    reinstalei as bibliotecas do python no venv

--- opt/md_tracker_search_provider/venv/bin/activate.fish
+++ opt/md_tracker_search_provider/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marciomr/MeusProgramas/md-tracker-search-provider/usr/local/bin/venv"
+set -gx VIRTUAL_ENV "/home/marciomr/MeusProgramas/md-tracker-search-provider/opt/md_tracker_search_provider/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
